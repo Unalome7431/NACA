0dd42477ed4b955b3a92bb6b312b7ddeaa492a61
Velengio Deriksen Charles <velengio@gmail.com>
2026-03-09 16:28:54 +0700

    first commit

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { useRef, useState } from "react"
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+import { useGSAP } from "@gsap/react"
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false)
+  const navRef = useRef(null)
+
+  useGSAP(() => {
+    ScrollTrigger.create({
+      start: 500,
+      onEnter: () => {
+        gsap.to(navRef.current, {
+          color: '#768a20',
+          duration: 0.7
+        }),
+        gsap.to('.bar', {
+          backgroundColor: '#768a20',
+          duration: 0.01
+        })
+      },
+      onLeaveBack: () => {
+        gsap.to(navRef.current, {
+          color: '#FFFFFF',
+          duration: 0.7
+        }),
+        gsap.to('.bar', {
+          backgroundColor: '#FFFFFF',
+          duration: 0.01
+        })
+      }
+    })
+  }, {scope: navRef})
+
+  return (
+    <nav className="fixed w-full top-0 z-50 p-10 pb-0 flex flex-row text-white justify-between font-elsie items-center" ref={navRef}>
+      <span className="text-3xl relative z-50">NACA</span>
+
+      <div className="hidden flex-row gap-10 text-xl md:flex">
+        <a href="/">About</a>
+        <a href="/">Partnership</a>
+        <a href="/">FAQ</a>
+        <a href="/">Contact</a>
+      </div>
+
+      <div className="flex flex-col md:hidden space-y-1.5 cursor-pointer relative z-50" onClick={() => setIsOpen(!isOpen)}>
+        <div className={`bar w-8 h-[3px] bg-white rounded-full transition-all duration-700 ease-in-out ${isOpen ? 'rotate-45 translate-y-[9px]' : ''}`}></div>
+        <div className={`bar w-8 h-[3px] bg-white rounded-full transition-all duration-700 ease-in-out ${isOpen ? 'opacity-0 scale-x-0' : ''}`}></div>
+        <div className={`bar w-8 h-[3px] bg-white rounded-full transition-all duration-700 ease-in-out ${isOpen ? '-rotate-45 -translate-y-[9px]' : ''}`}></div>
+      </div>
+
+      <div className={`absolute w-screen h-[770px] rounded-[35vw] rounded-br-none bg-lime-950 -top-300 left-0 transition-transform duration-700 ease-in-out ${isOpen ? 'translate-y-170' : ''} md:hidden`}>
+        <div className="flex flex-col text-end absolute right-10 bottom-6 gap-3">
+          <a href="/">About</a>
+          <a href="/">Partnership</a>
+          <a href="/">FAQ</a>
+          <a href="/">Contact</a>
+        </div>
+      </div>
+    </nav>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Elsie, Quicksand } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const elsie = Elsie({
+  variable: "--font-elsie",
   subsets: ["latin"],
+  weight: "400"
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const quicksand = Quicksand({
+  variable: "--font-quicksand",
   subsets: ["latin"],
 });
 
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${elsie.variable} ${quicksand.variable} antialiased overflow-x-hidden`}
       >
         {children}
       </body>
