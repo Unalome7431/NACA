019e90e7b9173af208a08db76046e6c4ed9d644f
Velengio Deriksen Charles <velengio@gmail.com>
2026-03-19 22:47:17 +0700

    fix: added cursor pointer to button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -42,23 +42,12 @@ export default function Button({ className, position, variant, border, font, fon
   }
   return (
     <>
-      {/*<div className={cn(buttonVariants({border: border || "primary", size: size || "sixty", className}), position, "group relative h-fit flex justify-center items-center overflow-hidden border bg-white")}>
-        <div className={`invisible flex gap-3 ${icon ? 'm-0' : 'm-2 hsm:m-1 md:m-2 lg:m-3'} md:text-[clamp(0.9rem,1.5vw,1.3rem)] hsm:text-[clamp(0.7rem,1.1vw,2rem)] not-hsm:text-[clamp(0.9rem,1.7vw,2rem)] font-lato`}>
-          {children}
-          {icon && <span>{icon}</span>}
-        </div>
-        <button className={cn(buttonVariants({fontColor: fontColor || "primary"}), font, "z-10 absolute flex gap-3 scale-95 group-hover:font-semibold group-hover:scale-100 group-hover:cursor-pointer group-active:scale-100 md:text-[clamp(0.9rem,1.5vw,1.3rem)] hsm:text-[clamp(0.7rem,1.1vw,2rem)] not-hsm:text-[clamp(0.9rem,1.7vw,2rem)] font-lato")} onClick={handleClick} {...props}>
-          {children}
-          {icon && <span>{icon}</span>}
-        </button>
-        <div className={cn(buttonVariants({variant: variant || "primary"}), "absolute w-full h-20 translate-y-20 group-hover:translate-y-0 transition-all duration-600")}></div>
-      </div>*/}
-      <div className={cn(buttonVariants({border: border || "primary", size: size || "sixty", className}), position, "group relative h-fit flex justify-center items-center overflow-hidden border bg-white")}>
+      <div className={cn(buttonVariants({border: border || "primary", size: size || "sixty", className}), position, "group relative h-fit flex justify-center items-center overflow-hidden border bg-white hover:cursor-pointer")}>
         <div className={`invisible flex gap-3 ${icon ? 'm-0' : 'm-2 hsm:m-1 md:m-2 lg:m-3'} text-2sm md:text-md lg:text-lg xl:text-xl font-lato`}>
           {children}
           {icon && <span>{icon}</span>}
         </div>
-        <button className={cn(buttonVariants({fontColor: fontColor || "primary"}), font, "z-10 absolute flex gap-3 scale-95 group-hover:font-semibold group-hover:scale-100 group-hover:cursor-pointer group-active:scale-100 text-2sm md:text-md lg:text-lg xl:text-xl font-lato")} onClick={handleClick} {...props}>
+        <button className={cn(buttonVariants({fontColor: fontColor || "primary"}), font, "z-10 absolute flex gap-3 scale-95 group-hover:font-semibold group-hover:scale-100 hover:cursor-pointer group-active:scale-100 text-2sm md:text-md lg:text-lg xl:text-xl font-lato")} onClick={handleClick} {...props}>
           {children}
           {icon && <span>{icon}</span>}
         </button>
--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <div className="flex not-md:flex-col not-md:justify-center mt-15 gap-[clamp(2rem,20vw,6rem)]">
           <p className="text-white font-elsie md:text-[clamp(2rem,15vw,5rem)] not-md:text-[clamp(2rem,15vw,4.5rem)] xl:text-[clamp(3rem,5vw,5.5rem)] w-fit sm:w-100 md:w-80 short:text-[6vw]">The <span className="text-lime-900 bg-black">Green</span> Version <br className="hidden md:flex"/> to
           <br className="hidden not-sm:flex"/> Your <span className="text-black bg-lime-900">Coffee</span></p>
-          <p className="text-white drop-shadow-2xl drop-shadow-black font-lato text-[clamp(0.5rem,3vw,1.2rem)] w-fit ml-10 text-justify md:ml-30 lg:ml-80 xl:ml-120 content-end my-5">Elevate your daily ritual with our curated collection of authentic, imported matcha. From vibrant ceremonial grades to versatile culinary blends, experience the pure, antioxidant-rich essence of Japan in every whisk.</p>
+          <p className="text-white drop-shadow-2xl drop-shadow-black font-lato text-[clamp(0.5rem,3vw,1.2rem)] w-fit ml-10 text-justify md:ml-30 lg:ml-80 xl:ml-120 content-end my-5">Elevate your daily ritual with our curated collection of authentic, imported matcha. From vibrant ceremonial grades, experience the pure, antioxidant-rich essence of Japan in every whisk.</p>
         </div>
       </div>
 
